7fe58864923ecd4a4162fe4dcc59aa96fe108cd6
Biraja prasad Barik <kbarik153@gmail.com>
2025-11-05 12:16:53 +0530

    project strcture is ready

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup,find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author= "Biraja Barik",
+    author_email="kbarik153@gmail.com",
+    packages=find_packages()
+)
